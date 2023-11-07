82406e0a54dc1575bdffc34aa29b1e8b6565bf74
expert-tester <chunon0705@gmail.com>
2023-11-07 23:46:18 +0800

    main class

---
+++ src/testgit/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testgit;
+
+/**
+ *
+ * @author Chun On
+ */
+public class Main {
+    	public static void main(String[] args) {
+		
+		// static = modifier. A single copy of a variable/method is created and shared.
+		//			The class "owns" the static member
+		
+		Friend friend1 = new Friend("Sponegbob");
+		Friend friend2 = new Friend("Patrick");
+		Friend friend3 = new Friend("Patrick");
+		
+		System.out.println(Friend.numberOfFriends);
+	}
+}
