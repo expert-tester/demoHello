00e4b6e7479d8e4919c31bb45a507be05b5e5a79
expert-tester <chunon0705@gmail.com>
2023-11-07 23:46:09 +0800

    friend class

---
+++ src/testgit/Friend.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testgit;
+
+/**
+ *
+ * @author Chun On
+ */
+public class Friend {
+    	String name;
+	
+	static int numberOfFriends;
+	
+	Friend(String name){
+		this.name=name;
+		numberOfFriends++;
+	}	
+}
