0137d9401e29472dea925cc36ce6470ccf39f46f
expert-tester <chunon0705@gmail.com>
2023-11-08 00:18:21 +0800

    main update

--- src/testgit/Main.java
+++ src/testgit/Main.java
@@ -14,10 +14,12 @@ public class Main {
 		// static = modifier. A single copy of a variable/method is created and shared.
 		//			The class "owns" the static member
 		
-		Friend friend1 = new Friend("Sponegbob");
-		Friend friend2 = new Friend("Patrick");
-		Friend friend3 = new Friend("Patrick");
+		Friend friend1 = new Friend("Spongebob",12);
+		Friend friend2 = new Friend("Patrick",14);
+		Friend friend3 = new Friend("Patrick",16);
 		
 		System.out.println(Friend.numberOfFriends);
+                System.out.println(friend1.nameofFriends());
+                System.out.println(friend1.ageofFriends());
 	}
 }
