d0bef42e22ef1124391683f5bc4dfc7ac5e4ab46
expert-tester <chunon0705@gmail.com>
2023-11-08 00:34:14 +0800

    secondline back

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -14,7 +14,9 @@ public class TestGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         System.out.print("Hello, this is the first commit to initialize the repository.");
+        System.out.print("This is the second line");
         System.out.print("this is the third line actually.");
     }
     
