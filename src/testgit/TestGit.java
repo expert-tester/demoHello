5a0045c5dd6297e1943dc268e3729c8745b24b06
expert-tester <chunon0705@gmail.com>
2023-11-07 18:54:39 +0800

    hello

---
+++ src/testgit/TestGit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testgit;
+
+/**
+ *
+ * @author Chun On
+ */
+public class TestGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello, this is the first commit to initialize the repository.");
+    }
+    
+}
