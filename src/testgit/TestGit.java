b4c17df484df8a21a5b5c63756d2f1ba2a87d5b3
expert-tester <chunon0705@gmail.com>
2023-11-07 23:02:10 +0800

    Revert "secondLine"
    This reverts commit 98034d53033d7847e547f7eeef7b76cbe27d6ce9.

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -14,8 +14,7 @@ public class TestGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello, this is the first commit to initialize the repository.");
-        System.out.print("This is the second line");
+        System.out.print("Hello, this is the first commit to initialize the repository.");
     }
     
 }
